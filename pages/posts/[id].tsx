ea976790bd99f670357ab04b9a352a0d61fc8ec4
Glenn <a810.glenn@gmail.com>
2022-10-09 22:47:19 +0900

    getStaticPath 이쯤되니 뭔말인지 하나도 모루게따

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { getAllPostIds, getPostData, getSortedPostsData } from '../../lib/post'
+import Head from "next/head";
 
-const Posts = () => {
-  return(
+const Posts = ({
+  postData,
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) => {
+  return <div>
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+    <article>
+      <h1>{postData.title}</h1>
       <div>
-        [id]
+        {postData.date}
       </div>
-  )
+      <div dangerouslySetInnerHTML={{__html:postData.contentHtml}} />
+    </article>
+  </div>
 }
 
 export default Posts
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
